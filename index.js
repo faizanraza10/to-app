84d1835aa272d473cb0499038d904c56ddd77717
faizanraza10 <m.faizanraza10@gmail.com>
2024-08-12 15:27:22 +0530

    Created a beckend file

---
+++ index.js
@@ -0,0 +1,17 @@
+// write basic boilerplate code,
+// with express.josn() middleware
+const express = require('express')
+const app = express()
+
+
+app.post('/todo', function(req, res) {
+  
+})
+
+app.get('/todo', function(req, res) {
+  
+})
+
+app.put('/completed', function(req, res) {
+  
+})
